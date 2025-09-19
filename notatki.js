20f0fee9285211903b89072d0db5c1950751d133
Dawid108 <gustowskidawid108@gmail.com>
2025-09-19 20:43:55 +0200

    Update notatki.js

--- notatki.js
+++ notatki.js
@@ -1,42 +1,178 @@
 // Lista przykładowych notatek
 const notes = [
   {
-    subject: "Biologia",
-    date: "2025-09-19",
-    topic: "Budowa komórki",
+    subject: "Język Polski",
+    date: "2025-09-09",
+    topic: "Problem antysemityzmu w literaturze II połowy XIX w.",
     content: `
-      <strong>Najważniejsze elementy:</strong>
-      <ul>
-        <li>Błona komórkowa</li>
-        <li>Cytoplazma</li>
-        <li>Jądro komórkowe</li>
-      </ul>
+    <ol>
+        <li><strong>Pojęcia:</strong>
+            <ol type="a">
+                <li><strong><em>Antysemityzm</em></strong> oznaczał niechęć, wrogość lub uprzedzenia wobec Żydów, która była wtedy mocno widoczna w społeczeństwie polskim i europejskim.</li>
+                <li><strong><em>Asymilacja Żydów</em></strong> – przystosowanie i stworzenie warunków do życia dla ludności żydowskiej poprzez włączenie jej do społeczeństwa polskiego z zachowaniem odrębności kulturowej i religijnej.</li>
+            </ol>
+        </li>
+
+        <li><strong>Nowela jako gatunek literacki:</strong>
+            <ul>
+                <li>Utwór epicki pisany prozą,</li>
+                <li>Krótki i zwarty utwór,</li>
+                <li>Jednowątkowość,</li>
+                <li>Niewielka liczba bohaterów,</li>
+                <li><em>Narrator trzecioosobowy wszechwiedzący</em>,</li>
+                <li>Ścisła kompozycja – rygorystyczna budowa, zwykle: ekspozycja (wprowadzenie), zawiązanie akcji, rozwinięcie, punkt kulminacyjny, rozwiązanie akcji (puenta),</li>
+                <li>Zwięzłość i brak dygresji,</li>
+                <li>Puenta,</li>
+                <li>Funkcja dydaktyczna lub moralna.</li>
+            </ul>
+        </li>
+
+        <li><strong>Geneza noweli "Mendel Gdański" oraz kontekst historyczny:</strong><br>
+            Nowela powstaje w 1890 roku na proźbę Elizy Orzeszkowej, kiedy w Polsce wzrastają ruchy <em>antysemickie</em>, a Konopnicka apeluje do Polaków o <em>asymilację</em> z ludnością żydowską.
+        </li>
+
+        <li><strong>Treść noweli "Mendel Gdański" (fragmenty):</strong>
+            <ol type="a">
+                <li><em>Scharakteryzuj bohaterów występujących we fragmencie:</em>
+                    <ul>
+                        <li><strong>Mendel Gdański</strong> jest starym Żydem, który od wielu lat mieszka i pracuje w mieście – czuje się jego częścią. Jest pracowity i uczciwy, podkreśla, że wszystko zdobył własnymi rękami. To człowiek odważny i dumny, który staje w oknie naprzeciw rozwścieczonego tłumu, broniąc swej godności.</li>
+                        <li><strong>Gruby Zegarmistrz</strong> jest sąsiadem Mendla, od wielu lat go zna. Przynosi niepokojącą wieść, że „Żydów mają bić”, mówiąc o tym z pewną lekkością i obojętnością. Choć nie jest agresywny, jego słowa ujawniają stereotypy i brak głębszego współczucia wobec Żydów.</li>
+                        <li><strong>Kubuś</strong> jest wnukiem Mendla, uczącym się pilnie w izbie. Jest bezbronny i niewinny, a tłum uderza właśnie w niego – kamień trafia go w głowę. Symbolizuje przyszłość i nadzieję, ale też pokazuje bezlitosne okrucieństwo napastników.</li>
+                        <li><strong>Tłum</strong> jest dziki, pijany i brutalny – działa w sposób bezmyślny i okrutny. Nie kieruje się sprawiedliwością, lecz ślepą nienawiścią – atakuje wszystkich Żydów tylko za to, że są Żydami. Zamiast szacunku dla odwagi Mendla, tłum odczytuje jego postawę jako urągowisko i reaguje jeszcze większą agresją.</li>
+                        <li><strong>Kobiety</strong> są bardziej wrażliwe i litościwe niż reszta tłumu. Gdy widzą Mendla w oknie z wnukiem, zaczynają płakać. Wzruszają się także losem rannego Kubusia, rzucając się ku niemu, co pokazuje ich współczucie i instynkt macierzyński.</li>
+                    </ul>
+                </li>
+                <li><em>Opisz stosunek Polaków do Żydów:</em>
+                    <ul>
+                        <li><strong>Gruby zegarmistrz do starego Mendla:</strong><br>
+                            Zegarmistrz zna Mendla od lat, ale mimo tego uważa, że Żydzi zasługują na pobicie tylko za to, że są Żydami.<br>
+                            Mówi o tym lekko i obojętnie, jakby była to zwykła wiadomość, bez poczucia niesprawiedliwości.<br>
+                            Traktuje Mendla jako obcego, mimo że ten podkreśla, iż całe życie uczciwie pracował i czuje się częścią miasta.
+                        </li>
+                        <li><strong>Kobiety do starego Mendla i jego wnuka Kubusia:</strong><br>
+                            Gdy widzą Mendla i Kubusia w oknie, ogarnia je wzruszenie – reagują płaczem.<br>
+                            Gdy Kubuś zostaje trafiony kamieniem, kobiety natychmiast biegną mu na pomoc.<br>
+                            W przeciwieństwie do reszty tłumu, okazują współczucie i ludzką solidarność.
+                        </li>
+                        <li><strong>Tłum na ulicy do starego Mendla i Kubusia:</strong><br>
+                            Tłum jest pełen nienawiści – atakuje Mendla tylko dlatego, że jest Żydem.<br>
+                            Nie oszczędza nawet niewinnego dziecka – kamień trafia w głowę Kubusia.<br>
+                            Odczytuje odważną postawę Mendla nie jako apel do człowieczeństwa, lecz jako zniewagę, co wyzwala jeszcze większą agresję.
+                        </li>
+                    </ul>
+                </li>
+            </ol>
+        </li>
+
+        <li><strong>Puenta noweli:</strong><br>
+            Konopnicka ukazując drastyczną scenę ataku Polaków na Żyda i jego wnuka podkreśla, że <strong><em>antysemityzm</em></strong> prowadzi do nienawiści i agresji i woła o <em>asymilację</em> Polaków i Żydów, równe traktowanie i niedzielenie ludzi tylko ze względu na pochodzenie. Stary Mendel powiedział Zegarmistrzowi, że ludzi można podzielić tylko na <strong><em>dobrych i złych</em></strong>.
+        </li>
+    </ol>
     `
   },
   {
-    subject: "Matematyka",
-    date: "2025-09-18",
-    topic: "Funkcje kwadratowe",
+    subject: "Język Polski",
+    date: "2025-09-03",
+    topic: "Wiadomości wprowadzające do epoki pozytywizmu.",
     content: `
-      <ol>
-        <li>Postać ogólna: ax²+bx+c</li>
-        <li>Delta = b² - 4ac</li>
-        <li>Miejsce zerowe: x = (-b ± √Δ) / 2a</li>
+  <ol>
+    <li>
+      <strong>Pozytywizm</strong> – pojęcie pochodzi od łacińskiego słowa <em>positivus</em> 
+      i oznacza: oparty na faktach, uzasadniony, użyteczny. 
+      Jest to kierunek w filozofii i literaturze, zapoczątkowany przez Augusta Comte’a 
+      w II połowie XIX wieku. Nazwa epoki została przyjęta od kierunku filozoficznego, 
+      a główne hasło epoki to: <em>„Wiedzieć, aby przewidywać, aby móc”</em>.
+    </li>
+    <li>
+      <strong>Czas trwania epoki:</strong>
+      <ol type="a">
+        <li>Początek: 1864 – upadek powstania styczniowego.</li>
+        <li>Koniec: lata 1890–1895 – pojawienie się nowego pokolenia artystów.</li>
       </ol>
-    `
-  },
-  {
-    subject: "Historia",
-    date: "2025-09-17",
-    topic: "Rewolucja Francuska",
-    content: `
-      <p>Rozpoczęła się w <strong>1789 roku</strong>.</p>
-      <p>Przyczyny:</p>
-      <ul>
-        <li>Kryzys gospodarczy</li>
-        <li>Niezadowolenie społeczne</li>
-        <li>Nierówności stanowe</li>
-      </ul>
+    </li>
+    <li>
+      <strong>Podstawowe kierunki w sztuce pozytywizmu:</strong>
+      <ol type="a">
+        <li>
+          <strong>Realizm</strong> – podstawowy kierunek w sztuce pozytywizmu, 
+          charakteryzujący się jak najwierniejszym odwzorowaniem rzeczywistości 
+          w dziele sztuki (np. w literaturze).
+        </li>
+        <li>
+          <strong>Naturalizm</strong> – kierunek w sztuce pozytywizmu, 
+          wiernie odtwarzający ludzką naturę, ukazujący m.in. brzydotę człowieka, starość itp.
+        </li>
+      </ol>
+    </li>
+    <li>
+      <strong>Podstawowe społeczne hasła pozytywizmu:</strong>
+      <ol type="a">
+        <li>
+          <strong>Praca organiczna</strong> – pogląd zakładający, że społeczeństwo funkcjonuje 
+          i rozwija się jak żywy organizm, a instytucje i grupy społeczne są ze sobą powiązane 
+          jak organy w ludzkim organizmie.
+        </li>
+        <li>
+          <strong>Praca u podstaw</strong> – praca z najniższymi warstwami społecznymi 
+          poprzez podstawową edukację i podniesienie poziomu kultury, np. zakładanie szkół wiejskich, 
+          aby osoby mieszkające na wsi nauczyć elementarnej wiedzy – czytania i pisania.
+        </li>
+        <li>
+          <strong>Asymilacja Żydów</strong> – przystosowanie i stworzenie warunków do życia 
+          dla ludności żydowskiej poprzez włączenie jej do społeczeństwa polskiego 
+          z zachowaniem odrębności kulturowej i religijnej. 
+          Przeciwieństwem asymilacji jest antysemityzm.
+        </li>
+        <li>
+          <strong>Emancypacja kobiet</strong> – ruch społeczny dążący do równouprawnienia kobiet 
+          i mężczyzn, np. poprzez przygotowanie kobiet do pracy zawodowej.
+        </li>
+      </ol>
+    </li>
+    <li>
+      <strong>Filozofie pozytywistyczne:</strong>
+      <ol type="a">
+        <li>
+          <strong>Auguste Comte</strong> – twórca scjentyzmu, głosił hasło, 
+          że wiedzę o rzeczywistości dostarcza człowiekowi nauka, 
+          zwłaszcza nauki przyrodnicze i socjologia.
+        </li>
+        <li>
+          <strong>Herbert Spencer</strong> – popularyzator ewolucjonizmu Karola Darwina, 
+          twierdził, że tak jak człowiek rozwija się i doskonali, 
+          tak dzięki niemu rozwija się również całe społeczeństwo – twórca organicyzmu.
+        </li>
+        <li>
+          <strong>Hipolit Taine – twórca utylitaryzmu</strong>
+          Według tego poglądu wszystko, co tworzy człowiek, nawet dzieło sztuki,
+          powinno być użyteczne dla drugiego człowieka, czyli mieć znaczenie społeczne. <br>
+          <strong>Utylitaryzm = użyteczność.</strong>
+        </li>
+      </ol>
+    </li>
+    <li>
+      <strong>Życie codzienne i wynalazki w II połowie XIX wieku</strong>
+        <ul>
+          <li>Nastąpiła masowa migracja ludności do miast.</li>
+          <li>Pojawiły się nowe wynalazki ułatwiające życie: lampy gazowe, telegraf, telefon.</li>
+          <li>Popularne stały się nowe sposoby spędzania czasu, np. chodzenie do teatru, kawiarni, czytanie gazet.</li>
+          <li>Prasa rozwijała się dzięki codziennym dziennikom, tygodnikom i dwutygodnikom.</li>
+          <li>Powstały nowe formy publicystyki: felieton, kronika tygodniowa, reportaż podróżniczy.</li>
+          <li>W gazetach drukowano w odcinkach najbardziej znane powieści, np. <em>Lalkę</em> Bolesława Prusa.</li>
+          <li>Rozwijała się medycyna: Ludwik Pasteur opracował szczepionkę przeciw wściekliźnie,
+              a Maria Skłodowska-Curie odkryła promieniotwórczość.</li>
+        </ul>
+    </li>
+    <li>
+      <strong>Ubiór w II połowie XIX wieku</strong>
+        <ul>
+          <li><strong>Mężczyźni:</strong> garnitur, koszula, kamizelka, krawat, płaszcz, kapelusz.</li>
+          <li><strong>Kobiety:</strong> gorsety, krynoliny, długie suknie, kapelusze ozdobione piórami lub woalką.</li>
+          <li>Moda coraz bardziej podkreślała różnice między strojami damskimi i męskimi,
+              a ubiór stawał się także wyznacznikiem statusu społecznego.</li>
+        </ul>
+    </li>
+  </ol>
     `
   }
 ];
