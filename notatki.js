620efe15c17976a140eb171bf4d36eaa427293c6
Dawid108 <gustowskidawid108@gmail.com>
2025-09-19 21:56:05 +0200

    Update notatki.js

--- notatki.js
+++ notatki.js
@@ -1,5 +1,224 @@
 // Lista przykładowych notatek
-const notes = [
+const notes = [   
+ {
+    subject: "Biologia",
+    date: "2025-09-19",
+    topic: "Budowa i funkcje układu krwionośnego",
+    content: `
+  <ol>
+    <li>
+      <p><strong>1. Funkcja układu krwionośnego</strong></p>
+
+      <p>Transportuje krew, dostarcza tlen i składniki odżywcze, usuwa dwutlenek węgla i produkty przemiany materii.</p>
+
+      <p>Uczestniczy w rozprowadzaniu hormonów, regulacji temperatury i obronie organizmu.</p>
+    </li>
+
+    <li>
+      <p><strong>2. Budowa tętnic, żył i naczyń włosowatych</strong></p>
+
+      <ul>
+        <li><strong>Tętnice</strong> – grube, elastyczne ściany, prowadzą krew od serca.</li>
+        <li><strong>Żyły</strong> – cieńsze ściany, mają zastawki, prowadzą krew do serca.</li>
+        <li><strong>Naczynia włosowate</strong> – bardzo cienkie, umożliwiają wymianę gazów i substancji.</li>
+      </ul>
+    </li>
+
+    <li>
+      <p><strong>3. Budowa serca</strong></p>
+
+      <p>Serce ma cztery jamy: dwa przedsionki i dwie komory.</p>
+
+      <p>Posiada zastawki kierujące przepływem krwi.</p>
+
+      <p>Ściana serca: wsierdzie, mięsień sercowy, osierdzie.</p>
+    </li>
+  </ol>
+    `
+  },
+ {
+    subject: "Historia",
+    date: "2025-09-18",
+    topic: "Wyprawa tysiąca czerwonych koszul",
+    content: `
+  <p><strong>Wyprawa tysiąca czerwonych koszul</strong> była zbrojną akcją ochotników pod dowództwem Giuseppe Garibaldiego w 1860 roku. Jej nazwa wzięła się od charakterystycznych czerwonych koszul, które nosili uczestnicy. Oddziały Garibaldiego wylądowały na Sycylii, szybko opanowały wyspę, a następnie przeprawiły się do Kalabrii i ruszyły na Neapol. Do działań przyłączyła się armia piemoncka, dzięki czemu we wrześniu 1860 roku zdobyto Neapol. Późniejszy plebiscyt na południu Włoch potwierdził wolę mieszkańców dołączenia do procesu zjednoczenia Włoch.</p>
+    `
+  }, 
+ {
+    subject: "Geografia",
+    date: "2025-09-17",
+    topic: "Urbanizacja na świecie i funkcje miasta",
+    content: `
+    <ol>
+        <li><strong>Urbanizacja</strong> – proces wzrostu liczby ludności mieszkającej w miastach oraz rozwoju i powiększania się miast, a także przenikania miejskiego stylu życia na obszary wiejskie.</li>
+        <li><strong>Aglomeracja monocentryczna</strong> – zespół miejski skupiony wokół jednego dużego miasta (metropolii), np. Warszawa z otaczającymi ją miejscowościami.</li>
+        <li><strong>Aglomeracja policentryczna (konurbacja)</strong> – obszar miejski, w którym funkcjonuje kilka dużych, równorzędnych miast połączonych gospodarczo i komunikacyjnie, np. Górnośląsko-Zagłębiowska Metropolia.</li>
+        <li><strong>Megalopolis</strong> – ogromny obszar silnie zurbanizowany, składający się z wielu miast i aglomeracji, które się ze sobą łączą, np. BosWash w USA (od Bostonu po Waszyngton).</li>
+        <li><strong>Miasto</strong> – większe skupisko ludności, posiadające prawa miejskie, charakteryzujące się przewagą zatrudnienia w usługach i przemyśle oraz pełniące różnorodne funkcje gospodarcze, administracyjne i społeczne.</li>
+    </ol>
+
+    <hr>
+
+    <p><strong>Pozytywy rozwoju urbanizacji (5)</strong></p>
+    <ol>
+        <li>Rozwój gospodarki dzięki koncentracji przemysłu, usług i kapitału.</li>
+        <li>Lepszy dostęp do edukacji i szkolnictwa wyższego.</li>
+        <li>Szeroka oferta kulturalna i rozrywkowa (muzea, kina, teatry).</li>
+        <li>Rozwój komunikacji i infrastruktury technicznej (drogi, kolej, internet).</li>
+        <li>Większe możliwości zatrudnienia i rozwoju zawodowego.</li>
+    </ol>
+
+    <hr>
+
+    <p><strong>Negatywy rozwoju urbanizacji (5)</strong></p>
+    <ol>
+        <li>Przeludnienie i problemy mieszkaniowe w dużych miastach.</li>
+        <li>Zanieczyszczenie środowiska (powietrza, wody, hałas, odpady).</li>
+        <li>Wzrost kosztów życia i duże różnice społeczne.</li>
+        <li>Rozwój patologii społecznych (bezdomność, przestępczość).</li>
+        <li>Niszczenie terenów zielonych i rolniczych przez rozrastanie się miast (suburbanizacja).</li>
+    </ol>
+
+    <hr>
+
+    <p><strong>Funkcje miast (10)</strong></p>
+    <ol>
+        <li>Administracyjna – siedziba władz (np. Warszawa).</li>
+        <li>Gospodarcza – centra przemysłu, handlu, finansów.</li>
+        <li>Komunikacyjna – węzły transportowe (lotniska, porty, koleje).</li>
+        <li>Kulturalna – teatry, muzea, centra sztuki.</li>
+        <li>Naukowa – uniwersytety, instytuty badawcze.</li>
+        <li>Religijna – ośrodki kultu religijnego (np. Częstochowa).</li>
+        <li>Turystyczna – atrakcje, zabytki, kurorty.</li>
+        <li>Militarna – garnizony, bazy wojskowe.</li>
+        <li>Zdrowotna – uzdrowiska, szpitale specjalistyczne.</li>
+        <li>Rozrywkowa – stadiony, kluby, centra rozrywki.</li>
+    </ol>
+    `
+  },  
+ {
+    subject: "Geografia",
+    date: "2025-09-17",
+    topic: "Ludność i Osadnictwo",
+    content: `
+    <p><strong>Geografia.</strong></p>
+    <p><strong>Dział:</strong> ludność i osadnictwo.</p>
+    <p><strong>Tematy:</strong></p>
+    <ul>
+        <li>Urbanizacja na świecie,</li>
+        <li>Funkcje miasta.</li>
+    </ul>
+
+    <p><strong>Wyjaśnij pojęcia:</strong></p>
+    <ol>
+        <li>Urbanizacja.</li>
+        <li>Aglomeracja monocentryczna.</li>
+        <li>Aglomeracja policentryczna.</li>
+        <li>Megalopolis.</li>
+        <li>Miasto.</li>
+    </ol>
+
+    <p><strong>Wymień:</strong></p>
+    <ol>
+        <li>Pozytywy rozwoju urbanizacji (5).</li>
+        <li>Negatywy rozwoju urbanizacji (5).</li>
+        <li>Funkcje miast (10).</li>
+    </ol>
+    `
+  }, 
+ {
+    subject: "Język Polski",
+    date: "2025-09-17",
+    topic: "Charakterystyka Ignacego Rzeckiego",
+    content: `
+    <ul type="a">
+        <li>
+            <strong>Co myśli o miłości?</strong><br>
+            Ignacy Rzecki uważa miłość za uczucie niezwykle ważne, ale też ulotne i trudne do osiągnięcia. Sam nigdy nie założył rodziny, pozostał samotnikiem. Widział w miłości raczej ideał romantyczny niż realne doświadczenie, przez co traktował ją z nostalgią.
+        </li>
+        <li>
+            <strong>Jaki jest jego stosunek do Napoleona?</strong><br>
+            Rzecki jest wielkim entuzjastą Napoleona Bonaparte i całej idei napoleońskiej. Uważa cesarza za bohatera, symbol walki o wolność i sprawiedliwość. Jego młodzieńcze ideały pozostały żywe do końca życia i często kierowały jego sposobem myślenia o świecie.
+        </li>
+        <li>
+            <strong>Jak wspomina udział w Wiośnie Ludów i Powstaniu Styczniowym?</strong><br>
+            Z ogromną tęsknotą wspomina lata młodości, gdy brał udział w wydarzeniach rewolucyjnych. Walczył w Wiośnie Ludów, a także wspierał Polaków podczas Powstania Styczniowego. Pamięta te czasy jako pełne nadziei i poświęcenia, mimo że zakończyły się klęską.
+        </li>
+        <li>
+            <strong>Jaki ma stosunek do ludzi innych narodowości?</strong><br>
+            Rzecki odznacza się dużą tolerancją wobec obcokrajowców. W młodości miał wielu przyjaciół wśród Niemców i Węgrów, cenił ich kulturę i otwartość. Potrafił dostrzegać w nich ludzi o podobnych wartościach, co wynikało z jego doświadczeń i patriotyzmu europejskiego.
+        </li>
+        <li>
+            <strong>Jakie ma marzenia wobec społeczeństwa polskiego?</strong><br>
+            Marzy o tym, aby Polacy byli narodem wolnym, silnym i solidarnym. Chciałby, aby społeczeństwo odrodziło się moralnie i gospodarczo, a młodsze pokolenie kontynuowało walkę o ojczyznę. Jego marzenia są idealistyczne, pełne patriotycznego romantyzmu.
+        </li>
+        <li>
+            <strong>Jaki jest jego stosunek do Stanisława Wokulskiego?</strong><br>
+            Ignacy darzy Wokulskiego ogromną przyjaźnią i szacunkiem. Traktuje go jak ucznia i niemal syna, w którego wierzy bezgranicznie. Broni go przed krytyką i nie rozumie w pełni jego działań, ale jest mu bezwarunkowo lojalny i oddany.
+        </li>
+        <li>
+            <strong>Jaki ma stosunek do pieniędzy?</strong><br>
+            Rzecki traktuje pieniądze jako narzędzie do życia, a nie cel sam w sobie. Jest człowiekiem skromnym, uczciwym i pracowitym. Nie przywiązuje wagi do bogactwa, lecz do wartości duchowych i moralnych, przez co stoi w opozycji do materializmu epoki.
+        </li>
+        <li>
+            <strong>Jak jest odbierany przez ludzi?</strong><br>
+            Ignacy Rzecki odbierany jest jako człowiek uczciwy, wierny i trochę staroświecki. Wzbudza szacunek swoją pracowitością i prawością, ale bywa postrzegany jako marzyciel oderwany od rzeczywistości. Mimo to cieszy się sympatią otoczenia, które docenia jego dobroć.
+        </li>
+    </ul>
+    `
+  }, 
+ {
+    subject: "Język Polski",
+    date: "2025-09-17",
+    topic: "Pytania Ignacy Rzecki",
+    content: `
+    <p><strong>Charakterystyka Ignacego Rzeckiego:</strong></p>
+    <p><strong>a) Co myśli o miłości?</strong></p>
+    <p><strong>b) Jaki jest jego stosunek do Napoleona?</strong></p>
+    <p><strong>c) Jak wspomina udział w Wiośnie Ludów i Powstaniu Styczniowym?</strong></p>
+    <p><strong>d) Jaki ma stosunek do ludzi innych narodowości?</strong></p>
+    <p><strong>e) Jakie ma marzenia wobec społeczeństwa polskiego?</strong></p>
+    <p><strong>f) Jaki jest jego stosunek do Stanisława Wokulskiego?</strong></p>
+    <p><strong>g) Jaki ma stosunek do pieniędzy?</strong></p>
+    <p><strong>h) Jak jest odbierany przez ludzi?</strong></p>
+    `
+  },
+ {
+    subject: "Język Polski",
+    date: "2025-09-15",
+    topic: "Charakterystyka bohaterów Lalki",
+    content: `
+    <ol>
+        <li>
+            <strong>Stanisław Wokulski</strong><br>
+            Wokulski to mężczyzna w średnim wieku, postawny, o szlachetnych rysach twarzy. 
+            Jest ambitny, pracowity i pełen determinacji, a jednocześnie wrażliwy i romantyczny. 
+            Dorobił się majątku na dostawach dla wojska, prowadził także sklep galanteryjny. 
+            Zakochany w Izabeli Łęckiej, próbuje zdobyć jej serce, inwestując w jej rodzinę i wchodząc do arystokracji. 
+            Choć odnosi sukcesy finansowe, cierpi przez nieszczęśliwą miłość, co prowadzi go do rozpaczy i zagubienia.
+        </li>
+
+        <li>
+            <strong>Ignacy Rzecki</strong><br>
+            Rzecki to starszy subiekt o siwych włosach i drobnej sylwetce. 
+            Jest lojalny, uczciwy i niezwykle oddany Wokulskiemu. 
+            Prowadzi sklep pod nieobecność właściciela, zachowując porządek i tradycyjne zasady handlu. 
+            W młodości walczył na Węgrzech, a jego serce zawsze biło dla idei wolności. 
+            Pisze pamiętnik, w którym wyraża swoje marzenia o odrodzeniu Napoleona i sprawiedliwszym świecie. 
+            Naiwnie wierzy, że Wokulski może połączyć arystokrację z mieszczaństwem i odmienić los ojczyzny.
+        </li>
+
+        <li>
+            <strong>Julian Ochocki</strong><br>
+            Ochocki to młody arystokrata o szczupłej budowie i inteligentnym spojrzeniu. 
+            Charakteryzuje go ciekawość świata, energia i pasja naukowa. 
+            Jest idealistą, pragnie poświęcić życie nauce i marzy o stworzeniu latających machin. 
+            Choć należy do arystokracji, nie interesują go bale ani konwenanse – szuka raczej ludzi światłych i otwartych. 
+            W powieści przyjaźni się z Wokulskim i wspiera go, podzielając jego poglądy na potrzebę postępu i rozwoju technicznego w Polsce.
+        </li>
+    </ol>
+    `
+  },
   {
     subject: "Język Polski",
     date: "2025-09-09",
