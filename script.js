6fcff42b4b7f44205009750ce31c3b1284800b52
Dawid108 <gustowskidawid108@gmail.com>
2025-12-20 14:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,5 +26,5 @@ if (historia108.length > 100) {
 localStorage.setItem("historia108", JSON.stringify(historia108));
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./20260106_sw.js');
+    navigator.serviceWorker.register('./20251203_sw.js');
 }
