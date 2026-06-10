89152e273b450403b03c708224acf36e01f01cea
Dawid108 <gustowskidawid108@gmail.com>
2026-06-10 20:25:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Punkty
+let dawid108 = parseInt(localStorage.getItem("dawid108")) || 0;
+
+// Dodawanie 1 punktu co 10 sekund
+setInterval(() => {
+    dawid108++;
+    localStorage.setItem("dawid108", dawid108);
+}, 10000);
+
+// Historia stron
 const fileName = window.location.pathname.split("/").pop() || "index.html";
 const pageUrl = window.location.href;
 
@@ -10,8 +20,10 @@ const dateString = now.toLocaleDateString("pl-PL");
 
 let historia108 = JSON.parse(localStorage.getItem("historia108")) || [];
 
+// Usunięcie poprzedniego wpisu tej samej strony
 historia108 = historia108.filter(item => item.file !== fileName);
 
+// Dodanie aktualnej strony
 historia108.push({
     file: fileName,
     name: namePart,
@@ -19,12 +31,14 @@ historia108.push({
     date: dateString
 });
 
-if (historia108.length > 100) {
-    historia108 = historia108.slice(historia108.length - 100);
+// Zachowanie tylko 10 ostatnich stron
+if (historia108.length > 10) {
+    historia108 = historia108.slice(-10);
 }
 
 localStorage.setItem("historia108", JSON.stringify(historia108));
 
+// Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./20251203_sw.js');
 }
