0ea1adb80a718d52cae0ffe7d817f094a4de92c6
Dawid108 <gustowskidawid108@gmail.com>
2026-06-10 22:12:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ localStorage.setItem("historia108", JSON.stringify(historia108));
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker.register('./20251203_sw.js');
 }
+
+// Czas
+let czas108 = localStorage.getItem("czas108") || 0;
+
+// Dodawanie 1 punktu co 5 sekund
+setInterval(() => {
+    czas108++;
+    localStorage.setItem("czas108", czas108);
+    console.log("Czas:", czas108);
+}, 1000);
